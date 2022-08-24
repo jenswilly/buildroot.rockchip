7485735a00748b39022ba1eaa9001f1b90b13d36
Jens Willy Johannsen <jens@jenswilly.dk>
2022-08-24 10:07:17 +0200

    Network

--- board/RK3308/post-build.sh
+++ board/RK3308/post-build.sh
@@ -1,9 +1,12 @@
 #!/bin/dash
 
 # fix eth0
-cnt=`grep -c eth0 $TARGET_DIR/etc/network/interfaces`
-if [ $cnt -eq "0" ]; then
-  echo "">> $TARGET_DIR/etc/network/interfaces
-  echo "auto eth0">> $TARGET_DIR/etc/network/interfaces
-  echo "iface eth0 inet dhcp">> $TARGET_DIR/etc/network/interfaces
-fi
+#cnt=`grep -c eth0 $TARGET_DIR/etc/network/interfaces`
+#if [ $cnt -eq "0" ]; then
+#  echo "">> $TARGET_DIR/etc/network/interfaces
+#  echo "auto eth0">> $TARGET_DIR/etc/network/interfaces
+#  echo "iface eth0 inet dhcp">> $TARGET_DIR/etc/network/interfaces
+#fi
+
+# Allow root SSH login
+# echo "\nPermitRootLogin yes\nPermitEmptyPasswords yes" >> $TARGET_DIR/etc/ssh/sshd_config
